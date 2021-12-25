d594f9b7cb09ef4d4798fa5005ead322f856ab7e
Ronnie Goodrich <kahootbird@gmail.com>
2021-12-24 23:30:53 -0500

    Singleton pattern

---
+++ singleton.js
@@ -0,0 +1,34 @@
+
+
+class Singleton {
+  constructor(options = {})
+  {
+    this.name = 'SingletonTester';
+    this.pointX = options.pointX || 10;
+    this.pointY = options.pointY || 12
+  }
+}
+//Single insance holder
+let instance;
+
+const SingletonTester = {
+  name: 'SingletonTester',
+
+  getInstance(options){
+    if(instance === undefined)
+    {
+      instance = new Singleton(options);
+    }
+    return instance;
+  }
+}
+
+
+  const singletonTest = SingletonTester.getInstance({
+    pointX: 10,
+    pointY: 15
+  });
+
+  // Log the output of pointX just to verify it is correct
+  // Outputs: 5
+  console.log(singletonTest.pointX + " " + singletonTest.pointY);
