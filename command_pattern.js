68944d6766e561b0a34de6734fd66ca9d1e8c909
Ronnie Goodrich <kahootbird@gmail.com>
2021-12-29 19:05:21 -0500

    Command pattern example

--- command_pattern.js
+++ command_pattern.js
@@ -1,34 +1,85 @@
-function Click() {
-    this.handlers = [];  // observers
+function add(x,y)
+{
+  return x+y
 }
-Click.prototype = {
-    subscribe: function (fn) {
-        this.handlers.push(fn);
-    },
-    unsubscribe: function (fn) {
-        this.handlers = this.handlers.filter(
-            function (item) {
-                if (item !== fn) {
-                    return item;
-                }
-            }
-        );
-    },
-    fire: function (o, thisObj) {
-        var scope = thisObj;
-        this.handlers.forEach(function (item) {
-            item.call(scope, o);
-        });
+function sub(x,y)
+{
+  return x-y
+}
+function mul(x,y)
+{
+  return x*y
+}
+function div(x,y)
+{
+  return x/y
+}
+
+var Command = function (execute, undo, value) {
+    this.execute = execute;
+    this.undo = undo;
+    this.value = value;
+}
+
+
+
+var AddCommand = function (value) {
+    return new Command(add, sub, value);
+};
+
+var SubCommand = function (value) {
+    return new Command(sub, add, value);
+};
+
+var MulCommand = function (value) {
+    return new Command(mul, div, value);
+};
+
+var DivCommand = function (value) {
+    return new Command(div, mul, value);
+};
+
+var Calculator = function () {
+    var current = 0;
+    var commands = [];
+
+    function action(command) {
+        var name = command.execute.toString().substr(9, 3);
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+
+    return {
+        execute: function (command) {
+            current = command.execute(current, command.value);
+            commands.push(command);
+            console.log(action(command) + ": " + command.value);
+        },
+
+        undo: function () {
+            var command = commands.pop();
+            current = command.undo(current, command.value);
+            console.log("Undo " + action(command) + ": " + command.value);
+        },
+
+        getCurrentValue: function () {
+            return current;
+        }
     }
 }
-//Run the code example using the click prototype class
-    var clickHandler = function (item) {
-        console.log("fired: " + item);
-    };
-    var click = new Click();
-    click.subscribe(clickHandler);
-    click.fire('event #1');
-    click.unsubscribe(clickHandler);
-    click.fire('event #2');
-    click.subscribe(clickHandler);
-    click.fire('event #3');
+
+    var calculator = new Calculator();
+    // issue commands
+    calculator.execute(new AddCommand(100));
+    console.log("Value: " + calculator.getCurrentValue());
+
+    calculator.execute(new SubCommand(24));
+    console.log("Value: " + calculator.getCurrentValue());
+
+    calculator.execute(new MulCommand(6));
+    console.log("Value: " + calculator.getCurrentValue());
+
+    calculator.execute(new DivCommand(2));
+    console.log("Value: " + calculator.getCurrentValue());
+    // reverse last two commands
+    calculator.undo();
+    calculator.undo();
